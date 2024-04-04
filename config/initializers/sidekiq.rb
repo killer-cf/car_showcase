eb62a4c84e91193228bffc82f2c099e2683d025d
killer-cf <costa.kilder@live.com>
2024-04-04 18:31:40 -0300

    edit redis db url

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: Rails.application.credentials.dig(Rails.env.to_sym, :redis, :url) }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: Rails.application.credentials.dig(Rails.env.to_sym, :redis, :url) }
 end
