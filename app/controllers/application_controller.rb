f4914ef3d752c39d43c9efbccd737aa4741bbacc
killer-cf <costa.kilder@live.com>
2024-04-24 17:05:31 -0300

    add metadata on cars list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,14 @@ class ApplicationController < ActionController::API
   def user_not_authorized
     render json: { error: 'Forbidden' }, status: :forbidden
   end
+
+  def pagination_dict(collection)
+    {
+      current_page: collection.current_page,
+      next_page: collection.next_page,
+      prev_page: collection.prev_page,
+      total_pages: collection.total_pages,
+      total_count: collection.total_count
+    }
+  end
 end
